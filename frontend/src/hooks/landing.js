8bf1a21aee7e444baeb176af69ddee4f6ce78cf3
Felix montanez <felix.montanez@gmail.com>
2024-12-31 18:09:38 -0800

    updated landing for including audio and separating episodes from literature

--- frontend/src/hooks/landing.js
+++ frontend/src/hooks/landing.js
@@ -44,24 +44,35 @@ const Landing = () => {
       <main role="main" className="inner cover">
         <h1 className="cover-heading">Story Driven Recovery</h1>
         <StyledParagraph>
-          Twelve Community is a spiritually-based, donation-welcomed social
-          platform with a mission to deliver high-quality audio recordings that
+          Twelve Community is a spiritually-based, donation-welcomed safe
+          space with a mission to deliver high-quality audio recordings that
           focus on recovery in various programs such as Alcoholics Anonymous,
           Codependents Anonymous and Adult Children of Alcoholic and
           Dysfunctional homes.
         </StyledParagraph>
+        <h1 className="text-left">Recorded Episodes</h1>
+        <h3 className="text-left">Shameless LA We Win Wednesdays</h3>
+        <StyledParagraph className="text-left">
+          These brief shares were recorded with permission from the Los Angleles ACA We Win Wednesdays meeting.
+        </StyledParagraph>
+        <StyledParagraph className="text-left">
+        <figure>
+          <figcaption>Episode One - ACA We Win Wednesdays, January 2024</figcaption>
+          <audio controls src="https://twelve-app.s3.us-west-1.amazonaws.com/EpisodeOne_ShamelessLA_January2024.mp3"></audio>
+        </figure>
+        </StyledParagraph>
+       <h1 className="text-left">Recorded Literature</h1>
+       <h3 className="text-left">A New Hope</h3>
+        <br/>
         <StyledParagraph>
-          Recordings are available in easy to digest formats with links for free
-          publications.
+          If you would like to hear more literature, please consider support through charitable donations, through
+          paypal or venmo.
         </StyledParagraph>
         <ImageContainer>
           <ImageIcons image={paypal} height="200px" />
           <ImageIcons image={venmo} height="200px" />
         </ImageContainer>
-        <StyledParagraph>
-          If you would like to support through donations, you can donate through
-          paypal or venmo.
-        </StyledParagraph>
+
       </main>
     </div>
   );
