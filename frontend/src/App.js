9ab21d08af513d01d187c47176b79f0ecbf41843
Felix montanez <felix.montanez@gmail.com>
2026-03-27 05:09:34 -0700

    updated front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,22 +10,27 @@ import Footer from "./components/Footer";
 import Landing from "./hooks/landing";
 import AddStory from "./hooks/addstory";
 import Login from "./hooks/login";
-import Register from './hooks/register';
- 
+import Register from "./hooks/register";
+
 import { AuthContainer } from "./contexts/auth";
 
 import "./styles/index.css";
 
 const App = () => {
-
   return (
     <AuthContainer isGlobal>
       <HelmetProvider>
         <Helmet>
-          <title>Twelve : Welcome</title>
+          <title>innerlight OS : Welcome</title>
           <link rel="canonical" href="http://www.twelve.community" />
-          <meta name="description" content="Twelve is a spiritual, community-based platform focused on anonymity, recovery, purpose, and connection through modern technology." />
-          <meta name="keywords" content="Twelve, recovery, spiritual, community, anonymity, connection, purpose, meetings, modern technology" />
+          <meta
+            name="description"
+            content="Twelve is a spiritual, community-based platform focused on anonymity, recovery, purpose, and connection through modern technology."
+          />
+          <meta
+            name="keywords"
+            content="Twelve, recovery, spiritual, community, anonymity, connection, purpose, meetings, modern technology"
+          />
         </Helmet>
         <ThemeProvider theme={lightTheme}>
           <Navbar />
@@ -34,7 +39,6 @@ const App = () => {
             <Login path="/login" />
             <Register path="/register" />
             <AddStory path="/addstory" />
-       
           </Router>
           <Footer />
         </ThemeProvider>
